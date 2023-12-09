8037e7258ff83abb6f9faf3b2c348a3ba0c2c64f
Пользователь <denis2000.kuzik@gmail.com>
2023-12-09 09:57:18 +0300

    wip

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,23 +1,18 @@
-// #include <QApplication>
+#include "main_window.hpp"
+#include <QApplication>
 #include <QDebug>
 #include <iostream>
-// #include <ostream>
+#include <qapplication.h>
 #include <string>
-// #include <qapplication.h>
 
-/* QCoreApplication *createApplication(int &argc, char *argv[]) {
-  for (int i = 1; i < argc; ++i) {
-    if (!qstrcmp(argv[i], "-debug"))
-      return new QCoreApplication(argc, argv);
-  }
-  return new QApplication(argc, argv);
-}
-*/
+
+
 #ifdef _WIN32
 #include <Windows.h>
 #endif
 
 int main(int argc, char **argv) {
+// attach console on win32
 #ifdef _WIN32
 if (AttachConsole(ATTACH_PARENT_PROCESS)) {
 
@@ -26,15 +21,10 @@ if (AttachConsole(ATTACH_PARENT_PROCESS)) {
 }
 #endif
 
-  std::cout << "this is from cout before qDebug!" << std::endl;
-  qDebug() << "qDebug before qapplication...";
-  std::cout << "this-is-from-cout" << std::endl;  
-  auto a = 11;
-  int t = 11;
-  std::cin >> t;
-  std::cout << t << std::endl;
-  std::cin >> t;
-
-  // return app->exec();
-  return t;
+  QApplication app{argc, argv};
+  
+  MainWindow mainWindow{};
+  mainWindow.show();
+ 
+  return app.exec();
 }
