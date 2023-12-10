7d05ced8426056c975468c77240320d1d92f56dd
Пользователь <denis2000.kuzik@gmail.com>
2023-12-10 23:05:51 +0300

    wip

--- gitlab-downloader-sources/src/app/main.cpp
+++ gitlab-downloader-sources/src/app/main.cpp
@@ -9,16 +9,16 @@
 
 int main(int argc, char **argv) {
 // attach console on win32
-#ifdef _WIN32
+#if defined(_WIN32) && defined(DEBUG)
   if (AttachConsole(ATTACH_PARENT_PROCESS)) {
 
     freopen("CONOUT$", "w", stdout);
     freopen("CONOUT$", "w", stderr);
   }
+  qDebug() << "RUNNING APP IN DEBUG MODE...";
 #endif
 
   QApplication app{argc, argv};
-
   MainWindow mainWindow{};
   mainWindow.show();
 
